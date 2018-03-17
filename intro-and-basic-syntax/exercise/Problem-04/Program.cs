af1e1cc1d89dcfd68cdfada3ff4effedade777de
maya <maya.ema@gmail.com>
2018-03-17 19:58:58 +0200

    Adding problem - 04

---
+++ intro-and-basic-syntax/exercise/Problem-04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class Program
+    {
+        static void Main()
+        {
+            var productName = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energyPer100Ml = int.Parse(Console.ReadLine());
+            var sugarPer100Ml = int.Parse(Console.ReadLine());
+            // reading user input for product info
+
+            var totalEnergy = (energyPer100Ml * volume) / 100.0; 
+            var totalSugar = (sugarPer100Ml * volume) / 100.0; 
+
+            Console.WriteLine($"{volume}ml {productName}:");
+            Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
+
+        }
+    }
+}
